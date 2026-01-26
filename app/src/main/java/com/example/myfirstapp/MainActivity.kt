83192f218ab4d7fb321b5c59a7570ca3d0582bdf
Diego Alves de Oliveira <diego@Diegos-MacBook-Air-2.local>
2026-01-26 14:34:10 +0000

    first commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.myfirstapp
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mainLayout = findViewById<LinearLayout>(R.id.mainLayout)
+        val ediName = findViewById<EditText>(R.id.ediName)
+        val btnAction = findViewById<Button>(R.id.btnAction)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+
+        btnAction.setOnClickListener {
+         val name = ediName.text.toString()
+         if (name.isNotEmpty()){
+             val message = "Olá, $name! Bem vindo ao meu primeiro App"
+             val spannable = android.text.SpannableString(message)
+
+             val start = message.indexOf(name)
+             val end = start + name.length
+
+             spannable.setSpan(
+                 android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
+                 start,
+                 end,
+                 android.text.Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+             )
+             spannable.setSpan(
+                 android.text.style.ForegroundColorSpan(android.graphics.Color.parseColor("#1976D2")),
+                 start,
+                 end,
+                 android.text.Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+             )
+
+             txtResult.text=spannable
+             mainLayout.setBackgroundColor(Color.parseColor("#E3F2FD"))
+         }else{
+             txtResult.text = "Você precisa digitar um nome primeiro"
+             mainLayout.setBackgroundColor(Color.parseColor("#FFCDD2"))
+         }
+        }
+    }
+}
