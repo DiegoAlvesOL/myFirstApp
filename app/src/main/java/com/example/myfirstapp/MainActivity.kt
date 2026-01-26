bd5050845d61b53b47423692aaaed1d1e9623b7f
Diego Alves de Oliveira <diego@Diegos-MacBook-Air-2.local>
2026-01-26 15:54:33 +0000

    implementado o campo para capturar idade

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -19,33 +19,42 @@ class MainActivity : AppCompatActivity() {
 
         val mainLayout = findViewById<LinearLayout>(R.id.mainLayout)
         val ediName = findViewById<EditText>(R.id.ediName)
+        val ediAge = findViewById<EditText>(R.id.ediAge)
         val btnAction = findViewById<Button>(R.id.btnAction)
         val txtResult = findViewById<TextView>(R.id.txtResult)
 
         btnAction.setOnClickListener {
+
          val name = ediName.text.toString()
+         val age = ediAge.text.toString()
          if (name.isNotEmpty()){
-             val message = "Olá, $name! Bem vindo ao meu primeiro App"
-             val spannable = android.text.SpannableString(message)
+             if(age.isNotEmpty()){
+                 val message = "Olá, $name! Bem-vindo ao meu primeiro App. Sua idade é: $age"
 
-             val start = message.indexOf(name)
-             val end = start + name.length
+                 val spannable = android.text.SpannableString(message)
+                 val start = message.indexOf(name)
+                 val end = start + name.length
 
-             spannable.setSpan(
-                 android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
-                 start,
-                 end,
-                 android.text.Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-             )
-             spannable.setSpan(
-                 android.text.style.ForegroundColorSpan(android.graphics.Color.parseColor("#1976D2")),
-                 start,
-                 end,
-                 android.text.Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-             )
+                 spannable.setSpan(
+                     android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
+                     start,
+                     end,
+                     android.text.Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                 )
+                 spannable.setSpan(
+                     android.text.style.ForegroundColorSpan(android.graphics.Color.parseColor("#1976D2")),
+                     start,
+                     end,
+                     android.text.Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                 )
 
-             txtResult.text=spannable
-             mainLayout.setBackgroundColor(Color.parseColor("#E3F2FD"))
+                 txtResult.text=spannable
+                 mainLayout.setBackgroundColor(Color.parseColor("#E3F2FD"))
+
+             }else{
+                 txtResult.text = "Você precisa digitar sua ideade"
+                 mainLayout.setBackgroundColor(Color.parseColor("#f2ddf0"))
+             }
          }else{
              txtResult.text = "Você precisa digitar um nome primeiro"
              mainLayout.setBackgroundColor(Color.parseColor("#FFCDD2"))
